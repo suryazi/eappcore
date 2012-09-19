e02b022515b06377062de7286d17fd80c6e1e0fd
suryazi <suryazi@abg.com.sa>
2012-09-19 16:57:51 +0300

    Created unit test for JWSCLoc

--- test/unit/com/blagha/JWSCLocTests.groovy
+++ test/unit/com/blagha/JWSCLocTests.groovy
@@ -5,13 +5,19 @@ package com.blagha
 import grails.test.mixin.*
 import org.junit.*
 
+import grails.buildtestdata.mixin.Build
+
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @TestFor(JWSCLoc)
+@Build(JWSCLoc)
 class JWSCLocTests {
 
-    void testSomething() {
-       //fail "Implement me"
+    void testLocSave() {
+        JWSCLoc loc = JWSCLoc.build()
+        def foundloc=JWSCLoc.get(loc.id)
+        assertNotNull loc.id
+        assertNotNull loc.loc
     }
 }
