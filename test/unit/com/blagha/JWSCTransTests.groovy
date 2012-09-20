dd80a7cfe276cea117711620ebac55ce0aceff18
suryazi <suryazi@abg.com.sa>
2012-09-20 13:52:50 +0300

    Created unit test for JWSCTrans

--- test/unit/com/blagha/JWSCTransTests.groovy
+++ test/unit/com/blagha/JWSCTransTests.groovy
@@ -5,13 +5,20 @@ package com.blagha
 import grails.test.mixin.*
 import org.junit.*
 
+import grails.buildtestdata.mixin.Build
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @TestFor(JWSCTrans)
+@Build([JWSCTrans,JWSCSpares])
 class JWSCTransTests {
 
-    void testSomething() {
-       //fail "Implement me"
+    void testTransSave() {
+        JWSCTrans trans = JWSCTrans.build(spares:JWSCSpares.build())
+        assertNotNull trans.id
+        assertNull trans.db
+        assertNull trans.cr
+        assertNull trans.bal
+        assertNull trans.rem
     }
 }
