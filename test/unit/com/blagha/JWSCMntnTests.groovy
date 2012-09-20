c6faebdfff4f9f78ebe777f6f463a52af77e0c96
suryazi <suryazi@abg.com.sa>
2012-09-20 13:01:15 +0300

    Created unit test for JWSCMntn

--- test/unit/com/blagha/JWSCMntnTests.groovy
+++ test/unit/com/blagha/JWSCMntnTests.groovy
@@ -5,13 +5,21 @@ package com.blagha
 import grails.test.mixin.*
 import org.junit.*
 
+import grails.buildtestdata.mixin.Build
+
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @TestFor(JWSCMntn)
+@Build(JWSCMntn)
 class JWSCMntnTests {
 
-    void testSomething() {
-       //fail "Implement me"
+    void testMntnSave() {
+        JWSCMntn mntn = JWSCMntn.build()
+        def foundmntn=JWSCMntn.get(mntn.id)
+        assertNotNull mntn.id
+        assertNotNull mntn.maintenance
+        assertNotNull mntn.dateCreated
+        assertNotNull mntn.lastUpdated
     }
 }
