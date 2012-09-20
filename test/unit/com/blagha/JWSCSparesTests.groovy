49c01ca67e9f155f3bf3fc887130a727b2ef4c94
suryazi <suryazi@abg.com.sa>
2012-09-20 14:39:34 +0300

    Created unit test for JWSCSpares

--- test/unit/com/blagha/JWSCSparesTests.groovy
+++ test/unit/com/blagha/JWSCSparesTests.groovy
@@ -5,13 +5,24 @@ package com.blagha
 import grails.test.mixin.*
 import org.junit.*
 
+import grails.buildtestdata.mixin.Build
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @TestFor(JWSCSpares)
+@Build([JWSCSpares,JWSCEquip,JWSCLoc,JWSCMfg,JWSCMntn,JWSCVessel])
 class JWSCSparesTests {
 
-    void testSomething() {
-       //fail "Implement me"
+    void testSparesSave() {
+        JWSCSpares spares = JWSCSpares.build(equipment:JWSCEquip.build(),loc:JWSCLoc.build(),manufacturer:JWSCMfg.build(),maintenance:JWSCMntn.build(),vessel:JWSCVessel.build())
+        assertNotNull spares.id
+        assertNull spares.itemNo
+        assertNull spares.partNo
+        assertNull spares.dsc
+        assertNotNull spares.qty
+        assertNull spares.rol
+        assertNotNull spares.uprice
+        assertNotNull spares.dateCreated
+        assertNotNull spares.lastUpdated
     }
 }
